3d971421f5d16fde70e86bba00247e26908ad354
eno <ryo.enomo@gmail.com>
2024-01-10 00:33:36 +0900

    スケジュールアプリの作成。CRUD処理readまで

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :start_at, :finish_at, :all_day, :memo))
+    if @post.save
+      flash[:notice] = "スケジュールを新規登録しました"
+      redirect_to :posts
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
